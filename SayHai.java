7dea43dd3dc7ae7ad456c22b3fa92f0037057a89
Priyanka Kalyampudi <kalyampudipriyanka@gmail.com>
2024-09-26 16:56:44 +0530

    Added SayHello.java assigned to me.

---
+++ SayHai.java
@@ -0,0 +1,8 @@
+class SayHai {
+
+	public static void main(String[] args) {
+		System.out.println("Hai");
+		
+	
+	}
+}
---
+++ SayHey.java
@@ -0,0 +1,8 @@
+class SayHey {
+
+	public static void main(String[] args) {
+		System.out.println("Hey");
+		
+	
+	}
+}
